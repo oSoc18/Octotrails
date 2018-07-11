fb01554afc93bf9ba33cebe56c69589da67fadcc
BramBleys <b.bleys@hotmail.com>
2018-07-11 09:59:23 +0200

    Add search function in stop controller

--- src/web-api/controllers/stops.controller.js
+++ src/web-api/controllers/stops.controller.js
@@ -21,4 +21,44 @@ function get(req, res) {
 //     .catch(e => next(e));
 // }
 
-export default { load, get };
+function search(req, res){
+  let by = req.query.by;
+  let term = req.query.term;
+
+  //TODO filter by the query params
+
+  return res.json({
+    "id": "811",
+		"descr_fr": "Georges Henri",
+		"descr_nl": "Georges_Henri",
+		"coord_x": 152592,
+		"coord_y": 170346,
+		"address": {
+			"fr": "Boulevard Brand Whitlock 92, Woluwe-Saint-Lambert",
+			"nl": "Boulevard Brand Whitlock 92, Sint-Lambrechts-Woluwe"
+		},
+		"lines": [
+			{
+				"line_id": 7,
+				"direction": {
+					"fr": "Heysel",
+					"nl": "Heysel"
+				},
+				"route_color": "FFF06E",
+				"route_text_color": "000000"
+			},
+			{
+				"line_id": 25,
+				"direction": {
+					"fr": "Rogier",
+					"nl": "Rogier"
+				},
+				"route_color": "991F36",
+				"route_text_color": "FFFFFF"
+			}
+		],
+		"type": 0
+  })
+}
+
+export default { load, get, search };
