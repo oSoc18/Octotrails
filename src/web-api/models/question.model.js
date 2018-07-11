5bd7593f2a25185c20a317a2de40fac44d704677
Dakad <3106338+Dakad@users.noreply.github.com>
2018-07-11 19:03:15 +0200

    Remove unnecessary sort

--- src/web-api/models/question.model.js
+++ src/web-api/models/question.model.js
@@ -62,9 +62,6 @@ QuestionSchema.statics = {
    */
   list({ skip = 0, limit = 50 } = {}) {
     return this.find({})
-      .sort({
-        createdAt: -1
-      })
       .skip(+skip)
       .limit(+limit)
       .exec();
