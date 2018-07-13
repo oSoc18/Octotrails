08d4a4c0a4f7c758511c9fbfab4d605fccb73039
Dakad <3106338+Dakad@users.noreply.github.com>
2018-07-13 13:42:12 +0200

    Use Stop validation in route

--- src/web-api/routes/stops.route.js
+++ src/web-api/routes/stops.route.js
@@ -1,11 +1,13 @@
 import express from 'express';
+import { checkSchema } from 'express-validator/check';
 
+import { Stops } from '../config/param-validation';
 import stopsCtrl from '../controllers/stops.controller';
 
 const router = express.Router();
 
 /** GET /api/stops/search - Get specific line by it number */
-router.get('/search', stopsCtrl.search);
+router.get('/search', checkSchema(Stops.getSearch), stopsCtrl.search);
 
 router.get('/proximity', stopsCtrl.getProximity);
 
