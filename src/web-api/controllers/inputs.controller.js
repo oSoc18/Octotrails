117fdf3f13be62da6147ed40d16be38d8ff7cd86
Dakad <3106338+Dakad@users.noreply.github.com>
2018-07-13 22:51:31 +0200

    Ajust  input controller

--- src/web-api/controllers/inputs.controller.js
+++ src/web-api/controllers/inputs.controller.js
@@ -3,6 +3,7 @@ import { validationResult, Result } from 'express-validator/check';
 
 import APIError from '../helpers/APIError';
 import Input from '../models/input.model';
+import History from '../models/history.model';
 
 /**
  * Check if the validatio has generated some errors.
@@ -19,14 +20,31 @@ const checkValidationErrors = function check(errors) {
 function saveInputs(req, res, next) {
   checkValidationErrors(validationResult(req));
 
-  const { stop_id, full } = req.params;
+  const { stop_id } = req.params;
   const { inputs } = req.body;
 
   // Create Input Documents
-  Input.create(inputs).then(inputDocs => {
-    const inputIDs = inputDocs.map(n => n._id);
-    res.json(inputDocs);
-  });
+  Input.create(inputs)
+    .then(inputDocs => inputDocs.map(n => n._id))
+    .then(inputIDs =>
+      History.getByStopId({ stop_id }).then(previous => {
+        // Create new history
+        const history = new History({
+          stop_id,
+          inputs: inputIDs
+        });
+
+        // ? Link to the previous history ?
+        if (previous) {
+          history.set({ previous: previous._id });
+        }
+        return history.save();
+      })
+    )
+    .then(nHistory => {
+      return res.json(nHistory);
+    })
+    .catch(e => new APIError('tet', 500));
 }
 
 export default { saveInputs };
