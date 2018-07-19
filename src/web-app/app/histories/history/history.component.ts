856ff4f3cba120064e9c0a2d86795a0356847d52
Dakad <3106338+Dakad@users.noreply.github.com>
2018-07-19 05:54:36 +0200

    Complete histoy.component

--- src/web-app/app/histories/history/history.component.ts
+++ src/web-app/app/histories/history/history.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Route, Router, ActivatedRoute } from '@angular/router';
+import { HistoryService } from '../histiries.service';
+import { History } from '../history';
 
 @Component({
   selector: 'app-history',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./history.component.css']
 })
 export class HistoryComponent implements OnInit {
+  stopId: string;
+  histories: History[];
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private historyService: HistoryService
+  ) {}
 
   ngOnInit() {
+    this.stopId = this.route.snapshot.queryParamMap.get('stop_id');
+    this.getStopHistories();
   }
 
+  getStopHistories(stopId?: string) {
+    return this.historyService
+      .getHistoriesByStopId(this.stopId)
+      .subscribe(list => (this.histories = list));
+  }
+
+  getHistory(historyId: string) {
+    return this.historyService
+      .getHistory(historyId)
+      .subscribe(history => (this.histories = [history]));
+  }
 }
