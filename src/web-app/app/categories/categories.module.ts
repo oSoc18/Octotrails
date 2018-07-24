cae82c564f0e81cac136593072f879f21828187f
BramBleys <b.bleys@hotmail.com>
2018-07-24 11:00:42 +0200

    Worked on categories

--- src/web-app/app/categories/categories.module.ts
+++ src/web-app/app/categories/categories.module.ts
@@ -4,12 +4,14 @@ import { SharedModule } from '../shared/shared.module';
 
 import { environment } from '../../environments/environment';
 import { CategoriesRoutingModule } from './categories-routing.module';
+import {MatExpansionModule} from '@angular/material/expansion';
+
 
 import { CategoriesOverviewComponent } from './overview/categories-overview.component';
 import { QuestionsOverviewComponent } from './questions/questions-overview.component';
 
 @NgModule({
-  imports: [SharedModule, CategoriesRoutingModule],
+  imports: [SharedModule, CategoriesRoutingModule, MatExpansionModule],
   declarations: [QuestionsOverviewComponent, CategoriesOverviewComponent]
 })
 export class CategoriesModule {}
--- src/web-app/app/categories/overview/categories-overview.component.ts
+++ src/web-app/app/categories/overview/categories-overview.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Category } from '../category';
+import { MAT_CHIPS_DEFAULT_OPTIONS } from '@angular/material';
 
 @Component({
   templateUrl: './categories-overview.component.html',
@@ -8,10 +9,39 @@ import { Category } from '../category';
 })
 export class CategoriesOverviewComponent implements OnInit {
   categories: Category[];
+  categories_group: any;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private router:Router) {}
 
   ngOnInit() {
     this.categories = this.route.snapshot.data['categories'];
+    this.categories_group = this.categories.reduce( (list, cat:Category) => {
+      const {num, parent_num} = cat;
+      const parent_cat = list[parent_num] || [];
+      if(parent_num !== null){
+        parent_cat.push(num);
+        list[parent_num] = parent_cat;
+      }else {
+        list[num] = []
+      }
+      return list;
+    } , {} );
   }
+
+  findSubCategory(subCategoryNum){
+    return this.categories.find(c => c.num === subCategoryNum) || {};
+  }
+
+  goToQuestion(categoryNum) {
+    this.router.navigate(['..'], {
+      relativeTo : this.route
+    });
+
+    /*
+    this.router.navigate(['./', categoryNum, 'questions'], {
+      relativeTo : this.route
+    });
+
+  */
+   }
 }
