5ba87c689a29508e0f2ab30e4aab95106415e316
Dakad <3106338+Dakad@users.noreply.github.com>
2018-07-24 19:01:45 +0200

    Add resolver && service fucntion to get specific category

--- src/web-app/app/categories/category.resolver.ts
+++ src/web-app/app/categories/category.resolver.ts
@@ -7,18 +7,18 @@ import {
 import { Observable, of } from 'rxjs';
 import { CategoryService } from './category.service';
 import { Category } from './category';
-import { Question } from './question';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CategoryResolver implements Resolve<Category[]> {
+export class CategoryResolver implements Resolve<Category> {
   constructor(private categoryService: CategoryService) {}
 
   resolve(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
-  ): Observable<Category[]> {
-    return this.categoryService.list();
+  ): Observable<Category> {
+    const category_num = route.paramMap.get('category_num');
+    return this.categoryService.getByNum(category_num);
   }
 }
