7d9fc19d0a80c30f72578e2596fa394e4e73d13d
evajacobs <eva.jacobs@student.howest.be>
2018-07-25 11:10:48 +0200

    loading icon

--- src/web-app/environments/environment.ts
+++ src/web-app/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   API_URL: 'http://localhost:8081/',
   mapbox: {
     accessToken:
-    '',
+    'pk.eyJ1Ijoicnlyb3YiLCJhIjoiY2prMHZybm0xMDVibzN0dDRnbHE1ZGliYyJ9.IdIjgTWgjgnjYNzQL9MzuQ',
     style: 'mapbox://styles/mapbox/light-v9'
   }
 };
