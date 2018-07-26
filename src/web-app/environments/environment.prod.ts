aac647a4e795e05206f76a1daec5ac084648a2eb
Dakad <3106338+Dakad@users.noreply.github.com>
2018-07-26 11:46:05 +0200

    Forget to set the mapbox token

--- src/web-app/environments/environment.prod.ts
+++ src/web-app/environments/environment.prod.ts
@@ -1,8 +1,9 @@
 export const environment = {
   production: true,
-  API_URL: '',
+  API_URL: 'https://qcontrol.be/',
   mapbox: {
-    accessToken: '',
+    accessToken:
+      'pk.eyJ1Ijoicnlyb3YiLCJhIjoiY2prMGJpbnhvMDRybjNwbXFrbnE5ZGVndyJ9._FyNDh3puw1YnF5Ur6bmqw',
     style: 'mapbox://styles/mapbox/light-v9'
   }
 };
--- src/web-app/environments/environment.ts
+++ src/web-app/environments/environment.ts
@@ -6,7 +6,8 @@ export const environment = {
   production: false,
   API_URL: 'http://localhost:8081/',
   mapbox: {
-    accessToken: 'pk.eyJ1Ijoicnlyb3YiLCJhIjoiY2prMHZybm0xMDVibzN0dDRnbHE1ZGliYyJ9.IdIjgTWgjgnjYNzQL9MzuQ',
+    accessToken:
+      'pk.eyJ1Ijoicnlyb3YiLCJhIjoiY2prMHZybm0xMDVibzN0dDRnbHE1ZGliYyJ9.IdIjgTWgjgnjYNzQL9MzuQ',
     style: 'mapbox://styles/mapbox/light-v9'
   }
 };
